149e0f722174133a96692bdd538a7a5b71b13092
simon <simon@richardson.nu>
2023-11-26 16:38:58 +0100

    update

--- nvim/.config/nvim/lua/config/alpha.lua
+++ nvim/.config/nvim/lua/config/alpha.lua
@@ -47,7 +47,7 @@ function M.setup()
   dashboard.section.buttons.val = {
     dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
     dashboard.button("c", "  Configuration", ":e $MYVIMRC <CR>"),
-    dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+    dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
   }
 
   local function footer()
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -6,6 +6,10 @@ function M.setup()
 
   -- packer.nvim configuration
   local conf = {
+		profile = {
+			enable = true,
+			threshold = 0, -- the amount in ms that a plugins load time must be over for it to be included in the profile
+		},
     display = {
       open_fn = function()
         return require("packer.util").float { border = "rounded" }
@@ -55,11 +59,39 @@ function M.setup()
     -- Git
     use {
       "TimUntersberger/neogit",
+			cmd = "Neogit",
       requires = "nvim-lua/plenary.nvim",
       config = function()
         require("config.neogit").setup()
       end,
     }
+
+		-- WhichKey
+		use {
+			 "folke/which-key.nvim",
+			 event = "VimEnter",
+			 config = function()
+				 require("config.whichkey").setup()
+			 end,
+		}
+
+		-- IndentLine
+		use {
+			"lukas-reineke/indent-blankline.nvim",
+			event = "BufReadPre",
+			config = function()
+				require("config.indentblankline").setup()
+			end,
+		}
+
+		-- Leap
+		use {
+			"ggandor/leap.nvim",
+			config = function()
+				require('leap').add_default_mappings()
+			end,
+		}
+
 
     if packer_bootstrap then
       print "Restart Neovim required after installation!"
